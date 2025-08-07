43dbb598aa203a08b93392e18d272dc89340bc2e
Hayes Chiasson <hchias2@lsu.edu>
2025-08-07 17:08:36 -0500

    Hero section setup (for navbar size context)

--- Haze-7/src/pages/Home.jsx
+++ Haze-7/src/pages/Home.jsx
@@ -7,7 +7,25 @@ function Home() {
             {/* Primary  */}
             
           <section className="bg-black-sand h-[80rem]">
-
+            {/* Hero Section */}
+            <div className="flex flex-col xl:flex-row pt-[10rem] h-[80vh] w-[80vw] mx-auto">
+              {/* Hero Name / Intro */}
+              <div className="text-center my-auto w-[50vw]">
+                <p className="text-[1.7rem] text-whiterock">
+                  Hello, I'm
+                </p>
+                <h1 className="text-[4rem]">
+                  Hayes Chiasson
+                </h1>
+                <p className="text-[2rem] text-akaroa">
+                  Fullstack Developer
+                </p>
+                </div>
+                {/* Professional Photo */}
+                <div className="flex mt-[3rem] ">
+                  <img src="./src/assets/haze-profile-pic.png" className="absolute h-[550px] w-[550px]"></img>
+                </div>
+            </div>
           </section>
           <section className="bg-akaroa bg-black-a h-[35rem] shadow-lg/50 shadow-gray">
             
