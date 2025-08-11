828b021a6bc9cf187446c1a6c3b037563f56d542
Hayes Chiasson <hayeschiasson1@yahoo.com>
2025-08-11 00:10:17 -0500

    Spread of toggle dark ./ light mode to whole app (mode/setmode)

--- Haze-7/src/App.jsx
+++ Haze-7/src/App.jsx
@@ -3,28 +3,34 @@ import './global.css'
 
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+import {useState} from 'react';
 
 //Images
-import Hlogo from '/H-logo.svg'
+import Hlogo from '/H-logo.svg';
 
 //Components
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 //Pages
 import Home from './pages/Home'
 
 function App() {
 
+  const [mode, setMode] = useState(false);
+
+
   return (
     <>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div data-theme={mode ? "light" : "dark"}>
+      <BrowserRouter>
+        <Navbar mode={mode} setMode={setMode} />
+        <Routes>
+          <Route path="/" element={<Home mode={mode} setMode={setMode} />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
     </>
   )
 }
--- Haze-7/src/pages/Home.jsx
+++ Haze-7/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar.jsx"
 
-function Home() {
+function Home({ mode, setMode }) {
     return (
       <>
+      {/* var(--primary-text-color) */}
+
         <div className="w-full">
             {/* Primary  */}
-          <section className="bg-black-sand h-[80rem] flex justify-center gap-[5rem]">
+          <section className="bg-sand h-[80rem] flex justify-center gap-[5rem]">
             {/* Hero Section */}
             <div className="flex lg:flex-row flex-col justify-center items-center gap-[3rem] lg:gap-28 h-auto lg:h-[95vh] pt-[10rem] lg:pt-0">
               {/* Hero Name / Intro */}
@@ -29,7 +31,7 @@ function Home() {
           <section className="about-bg h-[40rem] shadow-lg/50 shadow-gray">
             
           </section>
-          <section className="bg-black-sand h-[80rem]">
+          <section className="bg-sand h-[80rem]">
             
           </section>
         </div>
