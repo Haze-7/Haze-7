fed2872459932dea8b71eb55cc8cdc566f839fbd
Hayes Chiasson <hchias2@lsu.edu>
2025-08-21 12:43:43 -0500

    initial social media links / logo setup menu dropdown

--- Haze-7/src/components/Navbar.jsx
+++ Haze-7/src/components/Navbar.jsx
@@ -68,70 +68,64 @@ function Navbar({ mode, setMode }) {
                               {/* Column 1: Experience */}
                               <div className="dropdown-list">
                                 <div className="dropdown-list-header">
-                                  Experience
+                                  Work
                                 </div>
                                 <div className="dropdown-menu w-full">
                                   <div className="dropdown-menu-entry">
-                                    Experiences
+                                    Current Employment
                                   </div>
                                   <div className="dropdown-menu-entry ">
-                                    School
+                                    Previous Employment
                                   </div>
                                   <div className="dropdown-menu-entry">
-                                    Frontend
+                                    Relevant Skills
                                   </div>
-                                  <div className="dropdown-menu-entry">
+                                  {/* <div className="dropdown-menu-entry">
                                     Backend
                                   </div>
                                   <div className="dropdown-menu-entry">
                                     Experience 1
-                                  </div>
+                                  </div> */}
                                 </div>
                               </div>
-                              {/* Column 2: */}
+                              {/* Column 2: School */}
                               <div className="dropdown-list">
                                 <div className="dropdown-list-header">
-                                  Experience
+                                  Education
                                 </div>
                                 <div className="dropdown-menu w-full">
                                   <div className="dropdown-menu-entry">
-                                    Experiences
+                                    My Major
                                   </div>
                                   <div className="dropdown-menu-entry ">
-                                    School
+                                    My Minor
                                   </div>
                                   <div className="dropdown-menu-entry">
-                                    Frontend
+                                    Unofficial Transcript
                                   </div>
                                   <div className="dropdown-menu-entry">
-                                    Backend
+                                    Relevant Coursework
                                   </div>
                                   <div className="dropdown-menu-entry">
-                                    Experience 1
+                                    LSU
                                   </div>
                                 </div>
                               </div>
 
-                              {/* Column 3: ? */}
+                              {/* Column 3: Personal ? */}
                               <div className="dropdown-list">
                                 <div className="dropdown-list-header">
-                                  Experience 3
+                                  Personal
                                 </div>
                                 <div className="dropdown-menu w-full">
                                   <div className="dropdown-menu-entry">
-                                    Experiences
+                                    Interests
                                   </div>
                                   <div className="dropdown-menu-entry ">
-                                    School
+                                    Projects
                                   </div>
                                   <div className="dropdown-menu-entry">
-                                    Frontend
-                                  </div>
-                                  <div className="dropdown-menu-entry">
-                                    Backend
-                                  </div>
-                                  <div className="dropdown-menu-entry">
-                                    Experience 1
+                                    Future Plans
                                   </div>
                                 </div>
                               </div>
@@ -234,7 +228,103 @@ function Navbar({ mode, setMode }) {
                           <div className="navbar-dropdown-content">
                           {/* Left side Text Options */}
                             <div className="dropdown-title">
-                              Contact Me 2
+                            {/* Dropdown-list */}
+                            <div className="dropdown-list-container">
+                              {/* Column 1: Work Related */}
+                              <div className="dropdown-list">
+                                <div className="dropdown-list-header">
+                                  Work Related
+                                </div>
+                                <div className="dropdown-menu w-full">
+                                  <div className="dropdown-menu-entry">
+                                    Email: hchais2@lsu.edu
+                                  </div>
+                                  <div className="dropdown-menu-entry ">
+                                    Mobile Phone: (985) 438-2437
+                                  </div>
+                                  <div className="dropdown-menu-entry">
+                                    LinkedIn: (Link)/ ogo
+                                  </div>
+                                  {/* <div className="dropdown-menu-entry">
+                                    Backend
+                                  </div>
+                                  <div className="dropdown-menu-entry">
+                                    Experience 1
+                                  </div> */}
+                                </div>
+                              </div>
+                              {/* Column 2: Software Catalog / previous projects (Github, firebase, etc etc / websites) */}
+                              <div className="dropdown-list">
+                                <div className="dropdown-list-header">
+                                  Software Catalog
+                                </div>
+                                <div className="dropdown-menu w-full">
+                                  <Link to="https://github.com/Haze-7" className="dropdown-menu-logo-entry">
+                                    <div className={`p-[0.3rem]`}>
+                                      <div className="w-[2.5rem] h-[2.5rem] rounded-full">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                          <path
+                                            fill="var(--logo-fill-color)"
+                                            d="M237.9 461.4C237.9 463.4 235.6 465 232.7 465C229.4 465.3 227.1 463.7 227.1 461.4C227.1 459.4 229.4 457.8 232.3 457.8C235.3 457.5 237.9 459.1 237.9 461.4zM206.8 456.9C206.1 458.9 208.1 461.2 211.1 461.8C213.7 462.8 216.7 461.8 217.3 459.8C217.9 457.8 216 455.5 213 454.6C210.4 453.9 207.5 454.9 206.8 456.9zM251 455.2C248.1 455.9 246.1 457.8 246.4 460.1C246.7 462.1 249.3 463.4 252.3 462.7C255.2 462 257.2 460.1 256.9 458.1C256.6 456.2 253.9 454.9 251 455.2zM316.8 72C178.1 72 72 177.3 72 316C72 426.9 141.8 521.8 241.5 555.2C254.3 557.5 258.8 549.6 258.8 543.1C258.8 536.9 258.5 502.7 258.5 481.7C258.5 481.7 188.5 496.7 173.8 451.9C173.8 451.9 162.4 422.8 146 415.3C146 415.3 123.1 399.6 147.6 399.9C147.6 399.9 172.5 401.9 186.2 425.7C208.1 464.3 244.8 453.2 259.1 446.6C261.4 430.6 267.9 419.5 275.1 412.9C219.2 406.7 162.8 398.6 162.8 302.4C162.8 274.9 170.4 261.1 186.4 243.5C183.8 237 175.3 210.2 189 175.6C209.9 169.1 258 202.6 258 202.6C278 197 299.5 194.1 320.8 194.1C342.1 194.1 363.6 197 383.6 202.6C383.6 202.6 431.7 169 452.6 175.6C466.3 210.3 457.8 237 455.2 243.5C471.2 261.2 481 275 481 302.4C481 398.9 422.1 406.6 366.2 412.9C375.4 420.8 383.2 435.8 383.2 459.3C383.2 493 382.9 534.7 382.9 542.9C382.9 549.4 387.5 557.3 400.2 555C500.2 521.8 568 426.9 568 316C568 177.3 455.5 72 316.8 72zM169.2 416.9C167.9 417.9 168.2 420.2 169.9 422.1C171.5 423.7 173.8 424.4 175.1 423.1C176.4 422.1 176.1 419.8 174.4 417.9C172.8 416.3 170.5 415.6 169.2 416.9zM158.4 408.8C157.7 410.1 158.7 411.7 160.7 412.7C162.3 413.7 164.3 413.4 165 412C165.7 410.7 164.7 409.1 162.7 408.1C160.7 407.5 159.1 407.8 158.4 408.8zM190.8 444.4C189.2 445.7 189.8 448.7 192.1 450.6C194.4 452.9 197.3 453.2 198.6 451.6C199.9 450.3 199.3 447.3 197.3 445.4C195.1 443.1 192.1 442.8 190.8 444.4zM179.4 429.7C177.8 430.7 177.8 433.3 179.4 435.6C181 437.9 183.7 438.9 185 437.9C186.6 436.6 186.6 434 185 431.7C183.6 429.4 181 428.4 179.4 429.7z"
+                                          />
+                                        </svg>
+                                      </div>
+                                    </div>
+                                    Haze-7
+                                  </Link>
+                                  <div className="dropdown-menu-entry ">
+                                    My Minor
+                                  </div>
+                                  <div className="dropdown-menu-entry">
+                                    Relevant Coursework
+                                  </div>
+                                  <div className="dropdown-menu-entry">
+                                    LSU
+                                  </div>
+                                </div>
+                              </div>
+
+                              {/* Column 3: Social Media */}
+                              <div className="dropdown-list">
+                                <div className="dropdown-list-header">
+                                  Social Media
+                                </div>
+                                <div className="dropdown-menu w-full">
+                                  {/* replacde w/ instagram link */}
+                                  <Link to="/instagram " className="dropdown-menu-logo-entry">
+                                    <div className={`p-[0.5rem]`}>
+                                      <div className="w-[2.1rem] h-[2.1rem] pr-[0.2rem]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                          <path
+                                            fill="var(--logo-fill-color)"
+                                            d="M224.3 141a115 115 0 1 0 -.6 230 115 115 0 1 0 .6-230zm-.6 40.4a74.6 74.6 0 1 1 .6 149.2 74.6 74.6 0 1 1 -.6-149.2zm93.4-45.1a26.8 26.8 0 1 1 53.6 0 26.8 26.8 0 1 1 -53.6 0zm129.7 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM399 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+                                          />
+                                        </svg>
+                                      </div>
+                                    </div>
+                                    Hayes_Chiasson
+                                  </Link>
+                                  <div className="dropdown-menu-entry ">
+                                    Discord: Haze7_(logo / link)
+                                  </div>
+                                  <div className="dropdown-menu-entry">
+                                    Future Plans
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                              {/* Fill with Icons for different media (linked in, github, projects pages, etc) */}
+
+                      {/* <div className={` navbar-github-logo flex`}>
+                        <Link to="https://github.com/Haze-7" className="w-[3rem] h-[3rem] rounded-full">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                            <path
+                              fill="var(--logo-fill-color)"
+                              d="M237.9 461.4C237.9 463.4 235.6 465 232.7 465C229.4 465.3 227.1 463.7 227.1 461.4C227.1 459.4 229.4 457.8 232.3 457.8C235.3 457.5 237.9 459.1 237.9 461.4zM206.8 456.9C206.1 458.9 208.1 461.2 211.1 461.8C213.7 462.8 216.7 461.8 217.3 459.8C217.9 457.8 216 455.5 213 454.6C210.4 453.9 207.5 454.9 206.8 456.9zM251 455.2C248.1 455.9 246.1 457.8 246.4 460.1C246.7 462.1 249.3 463.4 252.3 462.7C255.2 462 257.2 460.1 256.9 458.1C256.6 456.2 253.9 454.9 251 455.2zM316.8 72C178.1 72 72 177.3 72 316C72 426.9 141.8 521.8 241.5 555.2C254.3 557.5 258.8 549.6 258.8 543.1C258.8 536.9 258.5 502.7 258.5 481.7C258.5 481.7 188.5 496.7 173.8 451.9C173.8 451.9 162.4 422.8 146 415.3C146 415.3 123.1 399.6 147.6 399.9C147.6 399.9 172.5 401.9 186.2 425.7C208.1 464.3 244.8 453.2 259.1 446.6C261.4 430.6 267.9 419.5 275.1 412.9C219.2 406.7 162.8 398.6 162.8 302.4C162.8 274.9 170.4 261.1 186.4 243.5C183.8 237 175.3 210.2 189 175.6C209.9 169.1 258 202.6 258 202.6C278 197 299.5 194.1 320.8 194.1C342.1 194.1 363.6 197 383.6 202.6C383.6 202.6 431.7 169 452.6 175.6C466.3 210.3 457.8 237 455.2 243.5C471.2 261.2 481 275 481 302.4C481 398.9 422.1 406.6 366.2 412.9C375.4 420.8 383.2 435.8 383.2 459.3C383.2 493 382.9 534.7 382.9 542.9C382.9 549.4 387.5 557.3 400.2 555C500.2 521.8 568 426.9 568 316C568 177.3 455.5 72 316.8 72zM169.2 416.9C167.9 417.9 168.2 420.2 169.9 422.1C171.5 423.7 173.8 424.4 175.1 423.1C176.4 422.1 176.1 419.8 174.4 417.9C172.8 416.3 170.5 415.6 169.2 416.9zM158.4 408.8C157.7 410.1 158.7 411.7 160.7 412.7C162.3 413.7 164.3 413.4 165 412C165.7 410.7 164.7 409.1 162.7 408.1C160.7 407.5 159.1 407.8 158.4 408.8zM190.8 444.4C189.2 445.7 189.8 448.7 192.1 450.6C194.4 452.9 197.3 453.2 198.6 451.6C199.9 450.3 199.3 447.3 197.3 445.4C195.1 443.1 192.1 442.8 190.8 444.4zM179.4 429.7C177.8 430.7 177.8 433.3 179.4 435.6C181 437.9 183.7 438.9 185 437.9C186.6 436.6 186.6 434 185 431.7C183.6 429.4 181 428.4 179.4 429.7z"
+                            />
+                          </svg>
+                        </Link>
+                      </div> */}
                             </div>
                           </div>
                         </div>                   
