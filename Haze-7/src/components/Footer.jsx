c57f4ba2a919d49f7921f91c64f91057cd90d9b9
Hayes Chiasson <hchias2@lsu.edu>
2026-01-08 16:19:26 -0600

    Temp Footer Setup (Based on CSG)

--- Haze-7/src/components/Footer.jsx
+++ Haze-7/src/components/Footer.jsx
@@ -1,12 +1,39 @@
+import {Link, useLocation} from "react-router-dom";
+import HLogo from "/H-logo.svg";
 
 function Footer() {
     return (
       <>
-        <div className="gradient-bg">
-            Footer
+        <div className="gradient-bg sm:h-[20vh] h-[50vh] flex mx-auto">
+          <div className="flex flex-col sm:flex-row sm:items-start items-center w-full pl-[2vw] pr-[5vw]">
+            {/* Logo */}
+              <a href="#Hero" className=" h-[3rem] w-[3rem] pt-8">
+                 <img src={HLogo} className=""></img>
+              </a>
+              {/* Grid / Links */}
+              <div className="grid grid-cols-1 sm:grid-cols-4 gap-y-[5vh] w-full text-center ml-auto sm:w-auto mt-[3vh] sm:mt-[5vh]">
+                {/* Home */}
+                <div>
+                  <h2 className="footer-heading font-semibold">
+                    Home
+                  </h2>
+                  <a href="/" className="footer-link hover:underline my-[1.5vh]">
+                    Temp
+                  </a>
+                  <a href="/" className="footer-link hover:underline">
+                    Temp
+                  </a>
+                </div>
+                {/* Repeat above To create Grid */}
+              </div>
+          </div>
         </div>
       </>
     )
 };
 
 export default Footer;
+
+              // <a href="#Hero" className=" h-[3rem] w-[3rem]">
+              //   <img src={HLogo} className=""></img>
+              // </a>
