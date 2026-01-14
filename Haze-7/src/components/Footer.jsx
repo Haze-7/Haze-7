c7e2ff36f8928aaf45687827df39e3aca49d8362
Hayes Chiasson <hayeschiasson1@yahoo.com>
2026-01-14 15:51:27 -0600

    Default Setup of detailed footer

--- Haze-7/src/components/Footer.jsx
+++ Haze-7/src/components/Footer.jsx
@@ -1,99 +1,64 @@
 import {Link, useLocation} from "react-router-dom";
-import HLogo from "/H-logo.svg";
+import HLogo from "/H-logo-full.svg";
 
 function Footer() {
     return (
       <>
-        <div className="gradient-bg sm:h-[30vh] h-[70vh] flex mx-auto">
-          <div className="flex flex-col sm:flex-row sm:items-start items-center w-full pl-[2vw] pr-[5vw]">
+        <div className="gradient-bg sm:h-[50vh] h-[70vh]">
+          {/*Content Container*/}
+          <div className="flex flex-col mx-[9rem]">
+            {/* Logo Row Container */}
+            <div className="">
             {/* Logo */}
-              <div className="h-full w-[30vw]">
-                <a href="#Hero" className="flex h-[10rem] w-[10rem] pt-8">
+              <div className="h-[20vh] w-[30vw] pt-8">
+                <a href="#Hero" className="flex h-[8.5rem] w-[8.5rem]">
                   <img src={HLogo} className=""></img>
                 </a>
               </div>
-              {/* Grid / Links */}
-              <div className="flex flex-col sm:flex-row w-auto gap-[5vw] text-center mt-[3vh] sm:mt-[5vh]">
-                {/* Home */}
-                <div>
-                  <h2 className="footer-heading font-semibold">
-                    Home
-                  </h2>
-                  {/* Container */}
-                  <div>
-                    {/* Items */}
-                    <div>
-                      <a href="/" className="footer-link hover:underline my-[1.5vh]">
-                        Temp
-                      </a>
-                    </div>
-                    <div>
-                      <a href="/" className="footer-link hover:underline">
-                        Temp
-                      </a>
-                    </div>
-                  </div>
+            </div>
+            {/* Footer Bar Top */}
+            <div className="flex footer-bar text-4xl w-full mt-[1rem] mb-[2rem]"> </div>
+            {/* Middle Content Container */}
+            <div className="sm:h-[20vh] w-full"> 
+              {/* Middle Content (Links) Grid */}
+              <div className="flex flex-col sm:flex-row">
+                {/*Column 1 */}
+                <div className="flex flex-col">
+                  <a href="/" className="footer-info font-semibold">
+                  Baton Rouge, La 70808
+                  </a>
+                  <a href="/" className="footer-info font-semibold">
+                  United States
+                  </a>
+                  <a href="/" className="footer-info font-semibold mt-[1rem]">
+                  hchias2@lsu.edu
+                  </a>
                 </div>
-                <div>
-                  <h2 className="footer-heading font-semibold">
-                    Home
-                  </h2>
-                  {/* Container */}
-                  <div>
-                    {/* Items */}
-                    <div>
-                      <a href="/" className="footer-link hover:underline my-[1.5vh]">
-                        Temp
-                      </a>
-                    </div>
-                    <div>
-                      <a href="/" className="footer-link hover:underline">
-                        Temp
-                      </a>
-                    </div>
-                  </div>
+                {/* Column 2 */}
+                <div className="flex flex-col ml-[10vw] md:ml-[25vw]">
+                  <a href="/" className="footer-info font-semibold">
+                    CSG Guide
+                  </a>
+                  <a href="/" className="footer-info font-semibold">
+                    Deadman's Hand
+                  </a>
+                  <a href="/" className="footer-info font-semibold">
+                    OrchestrAI
+                  </a>
                 </div>
-                <div>
-                  <h2 className="footer-heading font-semibold">
-                    Home
-                  </h2>
-                  {/* Container */}
-                  <div>
-                    {/* Items */}
-                    <div>
-                      <a href="/" className="footer-link hover:underline my-[1.5vh]">
-                        Temp
-                      </a>
-                    </div>
-                    <div>
-                      <a href="/" className="footer-link hover:underline">
-                        Temp
-                      </a>
-                    </div>
-                  </div>
+                {/* Column 3 */}
+                <div className="flex flex-col pl-[1vw]">
+                  <a href="/" className="footer-info font-semibold">
+                    BARBER
+                  </a>
+                  <a href="/" className="footer-info font-semibold">
+                    Suni: The Travel Companion
+                  </a>
                 </div>
-                <div>
-                  <h2 className="footer-heading font-semibold">
-                    Home
-                  </h2>
-                  {/* Container */}
-                  <div>
-                    {/* Items */}
-                    <div>
-                      <a href="/" className="footer-link hover:underline my-[1.5vh]">
-                        Temp
-                      </a>
-                    </div>
-                    <div>
-                      <a href="/" className="footer-link hover:underline">
-                        Temp
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-                {/* End Grid */}
               </div>
+            </div>
+            {/* Footer Bar Top */}
+            <div className="flex footer-bar text-4xl w-full my-[1rem]"> </div>
           </div>
         </div>
       </>
