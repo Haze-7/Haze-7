1fc64eebd86fd707d6f69495f17bee11e6b5f2de
Hayes Chiasson <hayeschiasson1@yahoo.com>
2026-01-20 14:07:05 -0600

    optimized Construction Page

--- Haze-7/src/pages/UnderConstruction.jsx
+++ Haze-7/src/pages/UnderConstruction.jsx
@@ -2,9 +2,12 @@
 function UnderConstruction() {
     return (
      <>
-      <div className="gradient-bg">
-        <h1>Under Construction</h1>
-        <p>This page is currently under construction. Please check back later.</p>
+     {/* Background / Wrapper */}
+      <div className="construction-container text-4xl">
+        <div className=" construction-instruction-container">
+          <h1 id="construction-title" className="font-bold">Under Construction!</h1>
+          <p id="construction-message" className="font-semibold">This page is currently under construction. Please check back later.</p>
+        </div>
       </div>
     </>
     );
